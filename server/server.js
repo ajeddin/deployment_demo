b46b17b2ecd2187458f91608872d940aaac147a9
ajeddin <abduljeddin@gmail.com>
2022-12-16 00:10:33 +0300

    done

--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
+require('dotenv').config()
 const express = require('express')
-const path = require('path')
 const app = express()
-
+const path = require('path')
+const cors = require('cors')
+const {ROLLBAR_TOKEN} = process.env
+app.use(express.json())
+app.use(cors())
 app.use(express.static('public'))
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'../public/index.html'))
 })
-app.listen(4545,()=>console.log('Listening on Port 4545'))
+// app.listen(4545,()=>console.log('Listening on Port 4545'))
+
+
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
